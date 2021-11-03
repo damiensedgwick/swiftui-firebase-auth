5e0228888a6445798ba453bcd224f8bcab54691e
Damien Sedgwick <damiensedgwick@icloud.com>
2021-11-03 09:40:48 +0000

    Initial Commit

---
+++ swiftui-firebase-auth/swiftui_firebase_authApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_firebase_authApp.swift
+//  swiftui-firebase-auth
+//
+//  Created by Damien K. Sedgwick on 03/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_firebase_authApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
